986a403ce1254441c03397a9b30121c8101b036a
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2024-09-23 13:05:42 +0200

    create user ıd && get user id

---
+++ src/random_bytes/random_bytes.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Ö. Haluk KARAKAYA on 23.09.2024.
+//
+
+#include "../../include/random_bytes/random_bytes.h"
+
+std::string generateRandomBytesHex(size_t length)
+{
+    // divide length to 2 so you can get character with the given size
+    auto byteLength = static_cast<size_t>(std::ceil(length / 2.0));
+
+    // create a vector for random bytes
+    std::vector<unsigned char> randomBytes(byteLength);
+
+    //random number generator
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, 255);
+
+    // fill all bytes with a random value
+    for ( auto& byte : randomBytes )
+    {
+        byte = static_cast<unsigned char>(dis(gen));
+    }
+
+    //convert bytes to hex string
+    std::stringstream ss;
+    for ( const auto& byte : randomBytes )
+    {
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int)byte;
+    }
+
+    return toUpperCase(ss.str()); // return value
+}
