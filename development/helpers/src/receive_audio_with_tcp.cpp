3025000cd396b175aedf408839ddc974e6dddc76
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2024-11-03 01:10:29 +0100

    :recycle: [Refactor] receive_audio_with_tcp.cpp cleaning

--- development/helpers/src/receive_audio_with_tcp.cpp
+++ development/helpers/src/receive_audio_with_tcp.cpp
@@ -1,36 +1,38 @@
 #include "../include/receive_audio_with_tcp.h"
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <string>
 
 void receive_audio_with_tcp(int client_socket) {
-    char buffer[1024];
+    constexpr size_t BUFFER_SIZE = 1024;
+    std::array<char, BUFFER_SIZE> buffer;
     std::ofstream audio_file("./received_audio.wav", std::ios::binary);
 
     if (!audio_file.is_open()) {
-        std::cerr << "Dosya açılamadı: ./received_audio.wav, Hata kodu: " << strerror(errno) << std::endl;
+        std::cerr << "Failed to open file: ./received_audio.wav, Error: " << strerror(errno) << std::endl;
         close(client_socket);
         return;
     }
 
-    bool receiving_audio = true;
-    size_t total_data_size = 0;
-
-    while (receiving_audio) {
-        ssize_t bytes_read = recv(client_socket, buffer, sizeof(buffer), 0);
+    size_t total_data_received = 0;
+    while (true) {
+        ssize_t bytes_read = recv(client_socket, buffer.data(), buffer.size(), 0);
 
         if (bytes_read > 0) {
-            audio_file.write(buffer, bytes_read);
-            total_data_size += bytes_read;
+            audio_file.write(buffer.data(), bytes_read);
+            total_data_received += bytes_read;
         } else if (bytes_read == 0) {
-            // Bağlantı kapandı
-            std::cout << "Connection closed by client." << std::endl;
-            receiving_audio = false;
+            // Connection closed gracefully by the client
+            std::cout << "Connection closed by client. Total data received: " << total_data_received << " bytes." << std::endl;
+            break;
         } else {
-            // Hata oluştu
+            // An error occurred during receiving
             std::cerr << "Error occurred while receiving data: " << strerror(errno) << std::endl;
-            receiving_audio = false;
+            break;
         }
     }
 
     audio_file.close();
     close(client_socket);
 }
-
