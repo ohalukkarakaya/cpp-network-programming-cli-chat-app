2ed179c976e79a90c81289d3a1571141c94fa2d5
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2024-11-03 01:23:44 +0100

    :recycle: [Refactor] process_incoming_command.cpp cleaning

--- development/helpers/src/process_incoming_command.cpp
+++ development/helpers/src/process_incoming_command.cpp
@@ -1,56 +1,61 @@
 #include "../include/process_incoming_command.h"
+#include <iostream>
 
-void process_incoming_command(RequestData& request_data)
-{
-    switch( request_data.get_command() )
-    {
+void handle_join_room(RequestData& request_data) {
+    if (request_data.get_message() == get_selected_room().get_room_id() &&
+        request_data.get_sender_id() != main_user_id) {
+        get_selected_room().user_joined(request_data.get_sender_id(), request_data.get_sender_ip());
+    }
+}
+
+void handle_leave_room(RequestData& request_data) {
+    if (request_data.get_message() == get_selected_room().get_room_id()) {
+        get_selected_room().user_left(request_data.get_sender_id());
+    }
+}
+
+void handle_message(RequestData& request_data) {
+    if (request_data.get_sender_id() != main_user_id) {
+        std::cout << BOLD_YELLOW << request_data.get_sender_id() << ": " << RESET
+                  << request_data.get_message() << ITALIC_LIGHT_GRAY << " - "
+                  << request_data.get_command_time() << RESET << std::endl;
+    }
+}
+
+void handle_update_ip(RequestData& request_data) {
+    if (request_data.get_sender_id() != main_user_id &&
+        request_data.get_message() == get_selected_room().get_room_id()) {
+        get_selected_room().update_member_ip(request_data.get_sender_id(), request_data.get_sender_ip());
+    }
+}
+
+void handle_audio(RequestData& request_data) {
+    if (request_data.get_sender_id() != main_user_id) {
+        std::cout << BOLD_YELLOW << request_data.get_sender_id() << ": " << RESET
+                  << "send an audio message..." << std::endl;
+    }
+}
+
+void process_incoming_command(RequestData& request_data) {
+    switch (request_data.get_command()) {
         case JOINROOM:
-        {
-            if( request_data.get_message() != get_selected_room().get_room_id() || request_data.get_sender_id() == main_user_id ) break;
-            get_selected_room().user_joined(request_data.get_sender_id(), request_data.get_sender_ip() );
+            handle_join_room(request_data);
             break;
-        }
         case LEAVEROOM:
-        {
-            if( request_data.get_message() != get_selected_room().get_room_id() ) break;
-            get_selected_room().user_left(request_data.get_sender_id());
+            handle_leave_room(request_data);
             break;
-        }
         case MESSAGE:
-        {
-            if( request_data.get_sender_id() != main_user_id )
-            {
-                std::cout << BOLD_YELLOW << request_data.get_sender_id() << ": " << RESET << request_data.get_message() << ITALIC_LIGHT_GRAY << " - " << request_data.get_command_time() << RESET << std::endl;
-            }
+            handle_message(request_data);
             break;
-        }
-        case NOTIFICATION:
-        {
-            break;
-        }
         case UPDATEIP:
-        {
-            if( request_data.get_sender_id() != main_user_id && request_data.get_message() == get_selected_room().get_room_id() )
-            {
-                get_selected_room().update_member_ip( request_data.get_sender_id(), request_data.get_sender_ip() );
-            }
+            handle_update_ip(request_data);
             break;
-        }
         case AUDIO:
-        {
-            if ( request_data.get_sender_id() != main_user_id ) {
-                std::cout << BOLD_YELLOW
-                          << request_data.get_sender_id()
-                          << ": " << RESET << "send an audio message..." << std::endl;
-            }
+            handle_audio(request_data);
             break;
-        }
-        case WHISPER:
-        {
-            break;
-        }
         case UNKNOWN:
         default:
+            // unknown commands are ignored
             break;
     }
 }
