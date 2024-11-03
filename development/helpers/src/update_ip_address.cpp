d34dd4cf3a7913a84032b5519079ad173e2a85b7
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2024-11-03 01:32:19 +0100

    :recycle: [Refactor] update_ip_address.cpp cleaning

--- development/helpers/src/update_ip_address.cpp
+++ development/helpers/src/update_ip_address.cpp
@@ -1,19 +1,25 @@
 #include "../include/update_ip_address.h"
 
-void update_ip_address( std::string new_ip_address )
-{
-    std::string command = get_command_as_string( UPDATEIP );
+void send_ip_update(const std::string& ip_address, const std::string& message, const std::string& command) {
+    int socket = send_with_tcp(LISTEN_PORT, ip_address, message, command);
+    if (socket != -1) {
+        close(socket);
+    } else {
+        std::cerr << "Failed to send IP update to " << ip_address << std::endl;
+    }
+}
+
+void update_ip_address(const std::string& new_ip_address) {
+    std::string command = get_command_as_string(UPDATEIP);
     std::string message_to_send = command + "/" + main_user_id + "/" + new_ip_address + "/" + get_selected_room().get_room_id();
 
-    int send_notification_server_sock = send_with_tcp(PORT, SERVERIP, message_to_send, command);
-    close(send_notification_server_sock);
+    // to server
+    send_ip_update(SERVERIP, message_to_send, command);
 
-    for( auto& member : get_selected_room().get_members() )
-    {
-        if(member.get_user_id() != main_user_id)
-        {
-            int send_notification_sock = send_with_tcp(LISTEN_PORT, member.get_user_ip(), message_to_send, command);
-            close(send_notification_sock);
+    // to members
+    for (const auto& member : get_selected_room().get_members()) {
+        if (member.get_user_id() != main_user_id) {
+            send_ip_update(member.get_user_ip(), message_to_send, command);
         }
     }
 }
